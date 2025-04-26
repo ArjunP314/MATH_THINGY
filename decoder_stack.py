290e1d50c0806f1b0a24019cce6fa6a1a94f5832
ArjunP314 <arjunpandey010209@gmail.com>
2025-04-26 00:21:06 -0400

    Create decoder_stack.py

---
+++ decoder_stack.py
@@ -0,0 +1,42 @@
+
+
+"""The decoder stack in inference mode."""
+
+from typing import Any, Tuple
+
+import gin  # type: ignore
+from transformer import decoder_stack  # type: ignore
+import transformer_layer as tl  # type: ignore
+
+
+struct = decoder_stack.struct
+nn_components = decoder_stack.nn_components
+position = decoder_stack.position
+jnp = decoder_stack.jnp
+attention = decoder_stack.attention
+
+DStackWindowState = decoder_stack.DStackWindowState
+
+Array = Any
+
+TransformerTaskConfig = decoder_stack.TransformerTaskConfig
+
+DStackDecoderState = Tuple[tl.DecoderState, ...]
+
+
+@gin.configurable
+class DecoderStackGenerate(decoder_stack.DecoderStack):
+  """Stack of transformer decoder layers."""
+
+  layer_factory = tl.TransformerLayerGenerate
+
+  def init_decoder_state_vanilla(
+      self, sequence_length: int, start_of_sequence: Array
+  ) -> DStackDecoderState:
+    """Return initial state for autoregressive generation."""
+    return tuple(
+        [
+            layer.init_decoder_state_vanilla(sequence_length, start_of_sequence)
+            for layer in self.transformer_layers
+        ]
+    )
